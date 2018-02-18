0d8752f7790fdfa9f4e2c1d5a7942409b05b25b9
dnguyensr <danielnguyensr@gmail.com>
2018-02-18 15:58:33 -0800

    Add views, update app

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require 'dotenv/load'
 require 'rubygems'
 require 'bundler'
+require_relative 'db'
+require_relative 'queries'
 
 Bundler.require
 
 class App < Sinatra::Base
 	get '/' do
-		return 'hello world'
-	end
+		erb :index
+  end
+
+  get '/vods' do
+    @vods =  DB::Queries.all_vods
+    erb :vods
+  end
 end
