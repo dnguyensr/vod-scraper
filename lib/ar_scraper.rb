ae51ae36a3474dfbc77a9a7c71f46ec40f98864e
dnguyensr <danielnguyensr@gmail.com>
2018-02-18 18:27:49 -0800

    Reformat, add clear_vods to queries, refactor and add db method to ar_scraper

--- lib/ar_scraper.rb
+++ lib/ar_scraper.rb
@@ -1,23 +1,35 @@
 require 'nokogiri'
 require 'open-uri'
-
+require_relative '../db'
+require_relative '../queries'
 
 def make_vod_url(url)
   return 'http://www.arirang.com/Player/Vod_Play.asp?MKey=' << url
 end
 
-def scrape(arr)
-  doc = Nokogiri::HTML(open('http://www.arirang.com/Tv2/Tv_VOD_Category.asp?PROG_CODE=TVCR0102&Menu_Code=101824'))
+def push_vods_to_db(doc)
   doc.css('.tv_vodlist li').each do |vod|
     vod_code = vod.css('h6').text.to_i
     vod_id = vod.css('a').attr('href').value.match(/'([^"]*)'/)[1]
     vod_cnt = vod.css('.vod_cnt')
     title = vod_cnt.at_css('strong').remove.text
     date = vod_cnt.inner_text
-    arr << [vod_code, make_vod_url(vod_id), title, date]
+    url = make_vod_url(vod_id)
+
+    create_vod(vod_code, url, title, date)
   end
 end
 
-arr = []
-scrape(arr)
-p arr
+def create_vod(id, url, title, date)
+  DB::Queries.create_vod(
+    id: id,
+    url: url,
+    title: title,
+    date: date
+  )
+end
+
+def scrape
+  doc = Nokogiri::HTML(open('http://www.arirang.com/Tv2/Tv_VOD_Category.asp?PROG_CODE=TVCR0102&Menu_Code=101824'))
+  push_vods_to_db(doc)
+end
--- queries.rb
+++ queries.rb
@@ -1,7 +1,13 @@
 # interpolation takes advantage of pg escaping values
 QUERIES = {
-  all_vods: %{select * from vods},
-  find_vods_by_date: %{select * from vods where date = :date},
-  find_vods_by_id: %{select * from vods where id = :id},
-  find_vods_by_title: %{select * from vods where title ~* :title},
+  all_vods: %{SELECT * FROM vods},
+  find_vods_by_date: %{SELECT * FROM vods WHERE date = :date},
+  find_vods_by_id: %{SELECT * FROM vods WHERE id = :id},
+  find_vods_by_title: %{SELECT * FROM vods WHERE title ~* :title},
+  create_vod: %{
+    INSERT INTO vods (id, url, title, date)
+    VALUES (:id, :url, :title, :date)
+    ON CONFLICT DO NOTHING
+  },
+  clear_vods: %{DELETE FROM vods},
 }
