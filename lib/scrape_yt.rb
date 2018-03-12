24ae8eab6fe5aa904aef32bafab5c936e7e00985
dnguyensr <danielnguyensr@gmail.com>
2018-03-11 23:20:57 -0700

    Add scrape_yt, scrape scripts, migration for yt table

---
+++ lib/scrape_yt.rb
@@ -0,0 +1,33 @@
+require_relative './scrape'
+
+def make_yt_url(url)
+  return 'https://www.youtube.com' << url
+end
+
+def get_ids_from_yt
+  DB::Queries.get_ids_from_yt
+end
+
+def scrape_arkpop_yt(args)
+
+  doc = open_url_html(args[:url])
+  unique_ids = get_ids_from_yt
+
+  doc.css("a:contains('#{args[:contains]}')").each do |vod|
+    if !vod.parent.parent.css('li')[1].text.include?('hour')
+      id = vod['href'][9..-1]
+      if !unique_ids.include?(id)
+        yt_url = make_yt_url(vod['href'])
+        title =  vod.text.encode("iso-8859-1").force_encoding("utf-8")
+        date = DateTime.now.strftime('%Y-%m-%d')
+        p [id, yt_url, title, date]
+        push_vod_to_yt(id, yt_url, title, date)
+      end
+    end
+  end
+end
+
+arkpop_args = {
+  url: 'https://www.youtube.com/user/arirangworld/videos',
+  contains: 'Pops in Seoul'
+}
---
+++ migrations/002-yt.sql
@@ -0,0 +1,6 @@
+create table yt (
+  id text primary key,
+  url text not null,
+  title text not null,
+  date date not null
+);
