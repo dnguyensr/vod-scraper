16bdf563fc959ba1b4180f20f708d570a7c5812f
dnguyensr <danielnguyensr@gmail.com>
2018-03-12 00:21:13 -0700

    add sinatra-contrib gem, views, update app with admin and yt namespace

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.5.0'
 
 gem 'puma'
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'dotenv'
 gem 'nokogiri'
 gem 'pg'
--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require 'dotenv/load'
 require 'rubygems'
 require 'bundler'
+require "sinatra/namespace"
 require_relative 'db'
 require_relative 'queries'
+require_relative 'lib/scrape_yt'
 require_relative 'lib/ar_scraper'
 
 Bundler.require
 
 class App < Sinatra::Base
   set :method_override, true
+  register Sinatra::Namespace
 
   get '/' do
     @vods =  DB::Queries.all_vods
@@ -18,6 +21,47 @@ class App < Sinatra::Base
   get '/vods' do
     @vods =  DB::Queries.all_vods
     erb :vods, :layout => :template
+  end
+
+  get '/yt' do
+    @vods =  DB::Queries.get_vods_from_yt
+    erb :yt, :layout => :template
+  end
+
+  namespace '/admin' do
+    get '/yt' do
+      redirect '/admin/yt/'
+    end
+    namespace '/yt' do
+      get '/' do
+        @vods =  DB::Queries.get_vods_from_yt
+        erb :'admin/admin_yt', :layout => :template
+      end
+
+      post '/scrape' do
+        arkpop_args = {
+          url: 'https://www.youtube.com/user/arirangworld/videos',
+          contains: 'Pops in Seoul'
+        }
+        scrape_arkpop_yt(arkpop_args)
+        redirect '/admin/yt/'
+      end
+
+      delete '/delete' do
+        DB::Queries.clear_yt
+        redirect '/admin/yt/'
+      end
+
+      get '/:id/edit' do
+        @vod =  DB::Queries.find_yt_by_id({id:  params[:id]})[0]
+        erb :'admin/admin_yt_edit', :layout => :template
+      end
+
+      delete '/:id' do
+        DB::Queries.delete_vod(id: params[:id])
+        redirect '/admin/yt/'
+      end
+    end
   end
 
   delete '/vods/:id' do
